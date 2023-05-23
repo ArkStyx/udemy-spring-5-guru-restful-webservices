f9d6eedd06a441cfe5b6a8188efd851d30d1decf
ArkStyx <regis.petit@gmail.com>
2023-05-24 00:29:49 +0200

    Commit premiers objets apres 1ere modifications de code

---
+++ src/main/java/guru/springframework/api/RestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+}
